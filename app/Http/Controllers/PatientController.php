8d2d551e3b49281579b4637deebac9a28a6db0dd
Zoki <zoolinom@gmail.com>
2018-01-30 21:25:00 +0100

    Added new route for patients Fixed footer to be on bottom

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -8,6 +8,6 @@ class PatientController extends Controller
 {
     public function index()
     {
-
+        return view('patients.index');
     }
 }
---
+++ resources/views/patients/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h2>Patients Table</h2>
+
+    <table class="table table-bordered">
+
+        <thead>
+
+            <tr>
+
+                <th class="col-lg-4">Patient name</th>
+
+                <th class="col-lg-4">Patient surname</th>
+
+                <th class="col-lg-4">JMBG</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody>
+
+            <tr>
+
+                <td class="col-lg-4">Patient name</td>
+
+                <td class="col-lg-4">Patient surname</td>
+
+                <td class="col-lg-4">JMBG</td>
+
+            </tr>
+
+
+            <tr>
+
+                <td class="col-lg-4">Patient name</td>
+
+                <td class="col-lg-4">Patient surname</td>
+
+                <td class="col-lg-4">JMBG</td>
+
+             </tr>
+
+        </tbody>
+
+    </table>
+
+@endsection
